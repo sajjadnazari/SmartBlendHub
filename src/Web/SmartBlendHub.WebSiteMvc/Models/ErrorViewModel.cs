4b55fdc57362cdae0e48148ff6fe822d0bf7798c
sajjad <sajjad69nazari@gmail.com>
2024-11-07 00:17:47 +0330

    init

---
+++ src/Web/SmartBlendHub.WebSiteMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartBlendHub.WebSiteMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
